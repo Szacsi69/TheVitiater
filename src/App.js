78b957479f7c28c34c91e69fc2f3dbd9a600cb9e
Szacsi69 <szabo01gergo@gmail.com>
2022-11-06 01:38:00 +0100

    Adding title screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import GameMaster from './game/GameMaster';
 import {colorTestLevel} from './game/levels';
 import {generate_lvl1} from './levels/level_generator';
+import { TitlePage } from './pages/TitlePage';
 
 function App() {
 
   return (
-    <div className="App container-fluid">
-      <GameMaster lvlCreate={generate_lvl1} />
+    <div className="base container-fluid">
+      <TitlePage />
     </div>
   );
 }
-
+//<GameMaster lvlCreate={generate_lvl1} />
 export default App;
---
+++ src/pages/TitlePage.js
@@ -0,0 +1,27 @@
+import '../css/base.css';
+import '../css/title.css';
+import titleImg from '../img/page_assets/title.png';
+import clickToStart from '../img/page_assets/clicktostart.png';
+import robot1 from '../img/robot/green_green_robot.png';
+import robot2 from '../img/robot/gray_gray_robot.png';
+import robot3 from '../img/robot/yellow_yellow_robot.png';
+
+import { motion } from "framer-motion/dist/framer-motion"
+
+export function TitlePage() {
+    return (
+        <div className="base container-fluid d-flex flex-column align-items-center pt-5">
+            <img className="title" alt="title" src={titleImg} />
+            <div>
+                <div className="start-div d-flex justify-content-center align-items-center m-5 p-3">
+                    <motion.img animate={{ opacity: 0 }} transition={{ ease: "linear", duration: 1.5, repeat: Infinity }} className="click-to-start-img" alt="clicktoplay" src={clickToStart} />
+                </div>
+                <div className="d-flex flex-row justify-content-around">
+                    <motion.img animate={{ rotate: 360 }} transition={{ ease: "linear", duration: 2, repeat: Infinity }}  className="robot-img m-2" alt="spinning robot" src={robot1} />
+                    <motion.img animate={{ rotate: 360 }} transition={{ ease: "linear", duration: 2, repeat: Infinity }} className="robot-img m-2" alt="spinning robot" src={robot2} />
+                    <motion.img animate={{ rotate: 360 }} transition={{ ease: "linear", duration: 2, repeat: Infinity }} className="robot-img m-2" alt="spinning robot" src={robot3} />
+                </div>
+            </div>
+        </div>
+    );
+}
